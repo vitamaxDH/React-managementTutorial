7eed4d4ec24f53b0eb71d630536af649192d01a9
vitamaxDH <hellonewtry@gmail.com>
2020-03-30 09:44:31 +0900

    ADD Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Customer from './components/customer'
+
+const customers = [
+  {
+    id : 1,
+    image : 'https://placeimg.com/64/64/1',
+    name : '홍길동',
+    bday : '921119',
+    gender : '남자',
+    job : '학생'
+  },
+  {
+    id : 2,
+    image : 'https://placeimg.com/64/64/2',
+    name : '베이직',
+    bday : '123423',
+    gender : '남자',
+    job : '천재'
+  },
+  {
+    id : 3,
+    image : 'https://placeimg.com/64/64/3',
+    name : '김갑순',
+    bday : '693424',
+    gender : '여자',
+    job : '바보'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-        <img src={logo} alt="logo" />
-        <h2>Let's develop management system</h2>
-    </div>
-  );
+    <React.Fragment>
+      {
+        customers.map(c=>{
+          return (
+            <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            bday={c.bday}
+            gender={c.gender}
+            job={c.job}
+            />
+          )
+        })
+      }
+
+   
+   
+    </React.Fragment>
+    );
 }
 
 export default App;
